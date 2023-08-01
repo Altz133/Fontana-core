fb66e2c5f4a94893e5a78f176428cecd81527314
Kristof <kmazurczak133@gmail.com>
2023-08-01 08:27:22 +0200

    FON-7 Created ("/login") endpoint with save endpoint.

--- src/main/java/com/fontana/backend/domain/controller/userController.java
+++ src/main/java/com/fontana/backend/domain/controller/userController.java
@@ -1,15 +1,15 @@
 package com.fontana.backend.domain.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.fontana.backend.domain.service.userService;
 import com.fontana.backend.domain.user.Users;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/user")
+@RestController
+@RequestMapping("fontana/api/v1/")
 @RequiredArgsConstructor
 public class userController {
 
