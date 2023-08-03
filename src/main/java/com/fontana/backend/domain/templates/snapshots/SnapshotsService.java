bb52f06e5cb5b247b903c65959dcdc95da964290
xNikodem <nikobacz@gmail.com>
2023-08-03 11:36:53 +0200

    Encje Schedules,Snapshots,Templates oraz wstępny endpoint snapshota do kontroli live controllera

---
+++ src/main/java/com/fontana/backend/domain/templates/snapshots/SnapshotsService.java
@@ -0,0 +1,11 @@
+package com.fontana.backend.domain.templates.snapshots;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SnapshotsService {
+
+    public void updateSnapshot(Integer index, Integer value) {
+        // Logic to update the snapshot value in the database
+    }
+}
