c7940de283b58eab1d163e6c45fbfee8acebc480
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-03 16:11:22 +0200

    Add firstName and lastName, manage user constructors with lombok

---
+++ src/main/java/com/fontana/backend/user/entity/User.java
@@ -0,0 +1,28 @@
+package com.fontana.backend.user.entity;
+
+import com.fontana.backend.role.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+    private String firstName;
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+
+}
