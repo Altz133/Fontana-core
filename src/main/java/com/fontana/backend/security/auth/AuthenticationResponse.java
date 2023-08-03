78682f259183c2ef0d5fa2766ff00042bd045c4f
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-03 12:33:44 +0200

    Create custom request and response object for authentication

---
+++ src/main/java/com/fontana/backend/security/auth/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.fontana.backend.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthenticationResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private LocalDateTime expiration;
+}
