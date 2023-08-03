c69d3c1d667501fbbce54e0d23a349c0b72d2bff
xNikodem <nikobacz@gmail.com>
2023-08-03 12:26:53 +0200

    Wartość globalna aktualnego stanu fontanny

--- src/main/java/com/fontana/backend/domain/templates/snapshots/SnapshotsService.java
+++ src/main/java/com/fontana/backend/domain/templates/snapshots/SnapshotsService.java
@@ -1,11 +1,24 @@
 package com.fontana.backend.domain.templates.snapshots;
 
+import com.fontana.backend.domain.globalVariables.CurrentFountainState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SnapshotsService {
+    private final CurrentFountainState currentFountainState;
+
+    @Autowired
+    public SnapshotsService(CurrentFountainState currentFountainState) {
+        this.currentFountainState = currentFountainState;
+    }
 
     public void updateSnapshot(Integer index, Integer value) {
-        // Logic to update the snapshot value in the database
+        if (index < 0 || index >= currentFountainState.getSnapshot().size()) {
+            // Logowanie błędu lub rzucenie wyjątku
+            return;
+        }
+        currentFountainState.getSnapshot().set(index, value);
+        // Możesz tutaj dodać również logikę aktualizacji wartości w bazie danych
     }
 }
