35181afe9b8f24a5cb8adeb82fe0e26649f6806d
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-03 12:33:22 +0200

    Create custom authentication filter * check if access token send via request header is not expired and validate access.

---
+++ src/main/java/com/fontana/backend/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.fontana.backend.security.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    private final JwtService jwtService;
+
+    /**
+     * Performs the authentication filter logic for processing incoming HTTP requests.
+     * This method is called by the framework when a request is received and is responsible
+     * for inspecting the HTTP Authorization header, extracting the JSON Web Token (JWT),
+     * and validating it using the provided JWT service.
+     *
+     * @param request     incoming HttpServletRequest object containing the request information.
+     * @param response    HttpServletResponse object for building and sending the response.
+     * @param filterChain FilterChain object used for invoking the next filters in the chain.
+     * @throws ServletException if any servlet-specific error occurs during the filter processing.
+     * @throws IOException      if an I/O error occurs during the filter processing.
+     */
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwt;
+
+        if (authHeader == null || !authHeader.startsWith(TOKEN_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+
+        if (SecurityContextHolder.getContext().getAuthentication() == null && jwtService.isTokenValid(jwt)) {
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    null, null, null
+            );
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
