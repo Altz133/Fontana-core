0d58b4580d0da0716a45630cb5388a572deb84bb
Miłosz Sobol <miloszsobolsci@gmail.com>
2023-08-06 19:10:43 +0200

    added service annotation to LightMapper

--- src/main/java/com/fontana/backend/devicesType/light/mapper/LightMapper.java
+++ src/main/java/com/fontana/backend/devicesType/light/mapper/LightMapper.java
@@ -1,9 +1,10 @@
 package com.fontana.backend.devicesType.light.mapper;
 
-import com.fontana.backend.devicesType.led.dto.LedDTO;
 import com.fontana.backend.devicesType.light.dto.LightDTO;
 import com.fontana.backend.devicesType.light.entity.Light;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LightMapper {
 
     //FIXME mapper
