3acd42f61b8738d92f3f04765c274aec87f5c753
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-07 15:18:16 +0200

    FON-47 Create separate config file for all rest endpoints

---
+++ src/main/java/com/fontana/backend/config/RestEndpoints.java
@@ -0,0 +1,17 @@
+package com.fontana.backend.config;
+
+public class RestEndpoints {
+
+    public static final String BASE = "/fontana/api/v1";
+
+    public static final String AUTH = BASE + "/auth";
+    public static final String AUTH_AUTHENTICATE = "/authenticate";
+    public static final String AUTH_REFRESHTOKEN = "/refreshtoken";
+
+    public static final String DMX = BASE + "/dmx";
+    public static final String DMX_UPDATE_JET = "/update/jet";
+    public static final String DMX_UPDATE_PUMP = "/update/pump";
+    public static final String DMX_UPDATE_LIGHT = "/update/light";
+    public static final String DMX_UPDATE_LED = "/update/led";
+    public static final String DMX_UPDATE_STATE = "/update/state";
+}
