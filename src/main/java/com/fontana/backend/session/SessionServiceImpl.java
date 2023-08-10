73655b72b4f51ecf329042061169d0e6d0b6e8b0
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-10 11:27:46 +0200

    FON-67 Refactor SessionServiceImpl class

--- src/main/java/com/fontana/backend/session/SessionServiceImpl.java
+++ src/main/java/com/fontana/backend/session/SessionServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDateTime;
 import java.util.List;
