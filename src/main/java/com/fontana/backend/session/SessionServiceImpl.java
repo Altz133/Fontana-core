232c35d62688f7236a5b9ab8fc5da6c44d7d5c9f
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-10 09:55:52 +0200

    FON-67 Validate creating new session * make sure any user (admin included for now) is not allowed to enter new session if any active session already exists in database

--- src/main/java/com/fontana/backend/session/SessionServiceImpl.java
+++ src/main/java/com/fontana/backend/session/SessionServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.fontana.backend.config.RestEndpoints.SESSION;
 
@@ -19,6 +21,7 @@ import static com.fontana.backend.config.RestEndpoints.SESSION;
 public class SessionServiceImpl implements SessionService {
 
     private static final String notFoundMsg = "Cannot found session with id:";
+    private static final String sessionBusyMsg = "Live control is busy";
 
     private final SessionRepository sessionRepository;
     private final SessionMapper sessionMapper;
@@ -36,13 +39,37 @@ public class SessionServiceImpl implements SessionService {
 
     @Override
     public ResponseEntity<?> add(SessionDTO sessionDTO) {
+        Session activeSession = getActiveSession();
+
+        if (activeSession != null) {
+            SessionBusyResponse response = SessionBusyResponse.builder()
+                    .message(sessionBusyMsg)
+                    .activeUserName(activeSession.getUsername())
+                    .activeSessionStartTime(activeSession.getOpenedTime())
+                    .intercepted(LocalDateTime.now())
+                    .build();
+
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
+        }
+
         Session saved = sessionRepository.save(sessionMapper.map(sessionDTO));
         log.info(saved.toString());
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.LOCATION, SESSION.concat("/").concat(saved.getId().toString()));
 
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).headers(headers).body(null);
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).build();
+    }
+
+    private Session getActiveSession() {
+        List<Session> activeSessions = sessionRepository.findAll().stream()
+                .filter(session -> session.getClosedTime() == null)
+                .toList();
+        if (activeSessions.size() >= 1) {
+            log.info(String.valueOf(activeSessions.get(0)));
+            return activeSessions.get(0);
+        }
+        return null;
     }
 
     @Override
