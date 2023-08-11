278dc52fa030358c43a3c46714a618d5b9a4e5fd
Deema <adammanczuk@gmail.com>
2023-08-11 12:54:32 +0200

    FON-89 fix

--- src/main/java/com/fontana/backend/template/TemplateRepository.java
+++ src/main/java/com/fontana/backend/template/TemplateRepository.java
@@ -1,6 +1,8 @@
 package com.fontana.backend.template;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemplateRepository extends JpaRepository<TemplateEntity,Integer> {
 }
