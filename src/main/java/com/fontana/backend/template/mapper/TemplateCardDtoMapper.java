7b87b383e9b9c1e514b271f8e183a97d5f0a1fb9
Deema <adammanczuk@gmail.com>
2023-08-13 20:09:28 +0200

    FON-99 added template length to TemplateCardDtoMapper

--- src/main/java/com/fontana/backend/template/mapper/TemplateCardDtoMapper.java
+++ src/main/java/com/fontana/backend/template/mapper/TemplateCardDtoMapper.java
@@ -18,8 +18,10 @@ public class TemplateCardDtoMapper {
                 .name(template.getName())
                 .ownerUsername(template.getUser().getUsername())
                 .favourites(templateService.getFavouriteCount(template))
-                .isFavouritedByOwner(templateService.isFavouritedByOwner(template)).updatedAt(template.getUpdatedAt())
+                .isFavouritedByOwner(templateService.isFavouritedByOwner(template))
+                .updatedAt(template.getUpdatedAt())
                 .status(template.getStatus())
+                .length(template.getSnapshotsSequence().size() / 4) //jeden snapshot co 0.25s
                 .build();
     }
 }
