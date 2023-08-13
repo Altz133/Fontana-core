5431276c059e8bbb58805679a1d37e4fc8497404
Deema <adammanczuk@gmail.com>
2023-08-13 20:48:17 +0200

    FON-90 added decorators for timestamps (creation and update)

--- src/main/java/com/fontana/backend/template/Template.java
+++ src/main/java/com/fontana/backend/template/Template.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -27,8 +29,10 @@ public class Template {
     @JoinColumn(name = "username")
     private User user;
 
+    @CreationTimestamp
     private Timestamp createdAt;
 
+    @UpdateTimestamp
     private Timestamp updatesAt;
 
     private int[] snapshotsSequence;
