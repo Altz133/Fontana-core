170a2d3c4a38c825cb412cca5d71f75593a3797b
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-14 20:52:53 +0200

    FON-105 Generate mock session and log when H2 is active.

--- src/main/java/com/fontana/backend/bootstrap/BootstrapInMemoryData.java
+++ src/main/java/com/fontana/backend/bootstrap/BootstrapInMemoryData.java
@@ -1,8 +1,12 @@
 package com.fontana.backend.bootstrap;
 
+import com.fontana.backend.log.entity.Log;
+import com.fontana.backend.log.repository.LogRepository;
 import com.fontana.backend.role.entity.Role;
 import com.fontana.backend.role.repository.RoleRepository;
 import com.fontana.backend.role.entity.RoleType;
+import com.fontana.backend.session.entity.Session;
+import com.fontana.backend.session.repository.SessionRepository;
 import com.fontana.backend.user.entity.User;
 import com.fontana.backend.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -10,6 +14,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * This class allows you to create a bunch of mock data and put them in H2 database on each run, so you always have
@@ -22,12 +29,16 @@ public class BootstrapInMemoryData implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final LogRepository logRepository;
+    private final SessionRepository sessionRepository;
 
     @Transactional
     @Override
     public void run(String... args) {
         loadTestRoles();
         loadTestUsers();
+        loadTestSession();
+        loadTestLogs();
     }
 
     private void loadTestRoles() {
@@ -70,5 +81,30 @@ public class BootstrapInMemoryData implements CommandLineRunner {
 
         userRepository.save(admin);
         userRepository.save(operator);
+    }
+
+    private void loadTestSession() {
+        Session session = Session.builder()
+                .username("fontanna_operator")
+                .openedTime(LocalDateTime.now())
+                .closedTime(null)
+                .expirationTime(LocalDateTime.now().plusMinutes(5))
+                .logs(new ArrayList<>())
+                .isForcedToClose(false)
+                .isAutoClosed(false)
+                .build();
+
+        sessionRepository.save(session);
+    }
+
+    private void loadTestLogs() {
+        Log first = Log.builder()
+                .deviceValue((short) 48)
+                .executedAt(LocalDateTime.now())
+                .user(userRepository.findAll().get(0))
+                .sessionId(sessionRepository.findAll().get(0).getId())
+                .build();
+
+        logRepository.save(first);
     }
 }
