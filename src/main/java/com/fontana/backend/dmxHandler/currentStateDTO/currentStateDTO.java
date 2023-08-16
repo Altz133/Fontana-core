8099e2d9df03752bb9949f17f62bab0482960de9
Miosz Sobol <miloszsobolsci@gmail.com>
2023-08-16 13:36:37 +0200

    added raw mapper for current state

---
+++ src/main/java/com/fontana/backend/dmxHandler/currentStateDTO/currentStateDTO.java
@@ -0,0 +1,11 @@
+package com.fontana.backend.dmxHandler.currentStateDTO;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class currentStateDTO {
+    Object[] currentStateArray;
+
+}
