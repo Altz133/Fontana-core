d5bcd5963a295c349f02af3130fbbb2372f28965
Adam Mańczuk <adammanczuk@gmail.com>
2023-08-17 10:13:59 +0200

    FON-99 template card dto mapper

---
+++ src/main/java/com/fontana/backend/template/mapper/TemplateCardDtoMapper.java
@@ -0,0 +1,18 @@
+package com.fontana.backend.template.mapper;
+
+import com.fontana.backend.template.dto.TemplateCardDto;
+import com.fontana.backend.template.entity.Template;
+import com.fontana.backend.template.service.TemplateServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TemplateCardDtoMapper {
+
+    private final TemplateServiceImpl templateService;
+
+    public TemplateCardDto TemplateToTemplateCardDto(Template template){
+        return TemplateCardDto.builder().name(template.getName()).ownerUsername(template.getUser().getUsername()).favourites(templateService.getFavouriteCount(template)).isFavouritedByOwner(templateService.isFavouritedByOwner(template)).updatedAt(template.getUpdatedAt()).status(template.getStatus()).build();
+    }
+}
