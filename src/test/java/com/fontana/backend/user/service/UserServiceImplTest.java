476fd27dcb376cd44f0baba564645bf3cd6a175a
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-17 10:29:57 +0200

    FON-126 Add test to `findAll` method

--- src/test/java/com/fontana/backend/user/service/UserServiceImplTest.java
+++ src/test/java/com/fontana/backend/user/service/UserServiceImplTest.java
@@ -1,12 +1,13 @@
 package com.fontana.backend.user.service;
 
+import com.fontana.backend.role.entity.Role;
 import com.fontana.backend.role.entity.RoleType;
 import com.fontana.backend.role.repository.RoleRepository;
 import com.fontana.backend.user.dto.UserDTO;
 import com.fontana.backend.user.entity.User;
 import com.fontana.backend.user.mapper.UserDtoMapper;
 import com.fontana.backend.user.repository.UserRepository;
-import com.fontana.backend.utils.AuthUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,16 +16,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Objects;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
+@Slf4j
 class UserServiceImplTest {
 
     @Mock
@@ -33,16 +35,25 @@ class UserServiceImplTest {
     @Mock
     private RoleRepository roleRepository;
 
+    @Mock
+    private UserDtoMapper userDtoMapper;
+
+    @InjectMocks
+    private UserServiceImpl userServiceImpl;
+
     private User testUser;
+    private UserDTO testUserDto;
+    private final List<UserDTO> mockUserDtos = new ArrayList<>();
+    private Role role;
+
     @BeforeEach
     public void init() {
-        testUser = User.builder()
-                .username("testUser")
-                .firstName("Test")
-                .lastName("User")
-                .role(roleRepository.findRoleByName(RoleType.OPERATOR.name()))
-                .logs(null)
-                .build();
+        testUser = initUser();
+
+        testUserDto = initUserDto();
+
+        mockUserDtos.add(testUserDto);
+        role = new Role();
     }
 
     @Test
@@ -51,5 +62,36 @@ class UserServiceImplTest {
 
         userRepository.save(testUser);
         verify(userRepository).save(testUser);
+    }
+
+    @Test
+    public void testFindAllUsers() {
+        role.setName(RoleType.OPERATOR.name());
+
+        when(userRepository.findAll()).thenReturn(new ArrayList<>(Collections.singletonList(testUser)));
+        when(userDtoMapper.map(any(User.class))).thenReturn(mockUserDtos.get(0));
+
+        List<UserDTO> result = userServiceImpl.findAll(null);
+
+        assertEquals(mockUserDtos.size(), result.size());
+    }
+
+    private User initUser() {
+        return User.builder()
+                .username("testUser")
+                .firstName("Test")
+                .lastName("User")
+                .role(roleRepository.findRoleByName(RoleType.OPERATOR.name()))
+                .logs(null)
+                .build();
+    }
+
+    private UserDTO initUserDto() {
+        return UserDTO.builder()
+                .username("testUser")
+                .firstName("Test")
+                .lastName("User")
+                .role(roleRepository.findRoleByName(RoleType.OPERATOR.name()))
+                .build();
     }
 }
