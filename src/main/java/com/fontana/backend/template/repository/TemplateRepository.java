2c0241fd7cda73981bfcdd566d0ca329246bd5d0
Adam Mańczuk <adammanczuk@gmail.com>
2023-08-17 10:27:57 +0200

    FON-99 added new method to template repository

--- src/main/java/com/fontana/backend/template/repository/TemplateRepository.java
+++ src/main/java/com/fontana/backend/template/repository/TemplateRepository.java
@@ -1,10 +1,12 @@
 package com.fontana.backend.template.repository;
 
 import com.fontana.backend.template.entity.Template;
+import com.fontana.backend.template.entity.TemplateStatus;
 import com.fontana.backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemplateRepository extends JpaRepository<Template, Integer> {
 
     Template[] getTemplatesByUser(User user);
+    Template[] getAllByStatus(TemplateStatus status);
 }
