c9f7ed040927a748cc3655831b9ca79299cb8f66
Adam Mańczuk <adammanczuk@gmail.com>
2023-08-23 15:48:18 +0200

    FON-120 basic controller without get

--- src/main/java/com/fontana/backend/schedule/controller/ScheduleController.java
+++ src/main/java/com/fontana/backend/schedule/controller/ScheduleController.java
@@ -1,10 +1,17 @@
 package com.fontana.backend.schedule.controller;
 
+import com.fontana.backend.schedule.dto.ScheduleCardDto;
+import com.fontana.backend.schedule.dto.ScheduleFormDto;
+import com.fontana.backend.schedule.mapper.ScheduleMapper;
 import com.fontana.backend.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.TimeZone;
 
 import static com.fontana.backend.config.RestEndpoints.*;
 
@@ -13,25 +20,31 @@ import static com.fontana.backend.config.RestEndpoints.*;
 @RequiredArgsConstructor
 public class ScheduleController {
     private final ScheduleService scheduleService;
+    private final ScheduleMapper scheduleMapper;
 
     @PostMapping(value = SCHEDULE_ADD)
-    public ResponseEntity<Object> addSchedule() {
-        return ResponseEntity.ok().build();
+    public void addSchedule(@RequestBody ScheduleFormDto scheduleFormDto) {
+        scheduleService.addSchedule(scheduleMapper.SchduleFormDtoToSchedule(scheduleFormDto));
     }
 
-    @PostMapping(value = SCHEDULE_DELETE)
-    public ResponseEntity<Object> deleteSchedule() {
-        return ResponseEntity.ok().build();
+    @DeleteMapping(value = SCHEDULE_DELETE)
+    public void deleteSchedule(@PathVariable Integer id) {
+        scheduleService.deleteSchedule(id);
     }
 
-    @PostMapping(value = SCHEDULE_UPDATE)
-    public ResponseEntity<Object> updateSchedule() {
-        return ResponseEntity.ok().build();
+    @PutMapping(value = SCHEDULE_UPDATE)
+    public void updateSchedule(@RequestBody ScheduleFormDto scheduleFormDto) {
+        scheduleService.updateSchedule(scheduleMapper.SchduleFormDtoToSchedule(scheduleFormDto));
     }
 
-    @PostMapping(value = SCHEDULE_GET)
-    public ResponseEntity<Object> getSchedule() {
-        return ResponseEntity.ok().build();
-    }
+//    @GetMapping(value = SCHEDULE_GET_MONTH)
+//    public List<Date> getDaysHavingSchedulesInTimestampRange(@RequestParam Date date, @RequestParam TimeZone timeZone) {
+//
+//    }
+//
+//    @GetMapping(value = SCHEDULE_GET_DAY)
+//    public List<ScheduleCardDto> getSchedulesInDay(@RequestParam Date date, @RequestParam TimeZone timeZone) {
+//
+//    }
 
 }
