b5f2890f83f6e50902eb832f42b8e289939a5071
Adam Mańczuk <adammanczuk@gmail.com>
2023-08-23 11:57:25 +0200

    FON-120 schedule cycle days enum fix

--- src/main/java/com/fontana/backend/schedule/entity/ScheduleCycleDays.java
+++ src/main/java/com/fontana/backend/schedule/entity/ScheduleCycleDays.java
@@ -1,11 +1,11 @@
 package com.fontana.backend.schedule.entity;
 
 public enum ScheduleCycleDays {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT,
+    SUN
 }
