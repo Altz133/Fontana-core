b94c3ee57417ddf3eceb50861f17bb114c0b1a52
Sebastian Mazur <seb.maz1996@gmail.com>
2023-08-23 14:23:52 +0200

    FON-192 Add logic to not allow adding the same user as watcher twice

--- src/main/java/com/fontana/backend/session/service/NotificationServiceImpl.java
+++ src/main/java/com/fontana/backend/session/service/NotificationServiceImpl.java
@@ -7,16 +7,22 @@ import com.fontana.backend.session.repository.SessionRepository;
 import com.fontana.backend.session.repository.SessionWatcherRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class NotificationServiceImpl implements NotificationService {
+
+    @Value("${notification.already-displayed}")
+    private String alreadyDisplayedMsg;
 
     private final SessionRepository sessionRepository;
     private final SessionWatcherRepository watcherRepository;
@@ -27,6 +33,14 @@ public class NotificationServiceImpl implements NotificationService {
 
         if (assignedSession.isEmpty()) {
             return ResponseEntity.notFound().build();
+        }
+
+        Session session = assignedSession.get();
+
+        if (containsWatcher(session, request.getUsername())) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", alreadyDisplayedMsg);
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
         }
 
         SessionWatcher watcher = buildSessionWatcher(sessionId, request);
@@ -45,5 +59,14 @@ public class NotificationServiceImpl implements NotificationService {
                 .sessionId(sessionId)
                 .watcher(request.getUsername())
                 .build();
+    }
+
+    private boolean containsWatcher(Session session, String username) {
+        for (SessionWatcher watcher : session.getWatchers()) {
+            if (watcher.getWatcher().equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
