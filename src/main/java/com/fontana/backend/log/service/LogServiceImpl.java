29a29d455fe1bb774af4857bf204525aebebd712
Oskar Pliżga <oskarplizga@outlook.com>
2023-08-25 14:13:21 +0200

    deleted unused import

--- src/main/java/com/fontana/backend/log/service/LogServiceImpl.java
+++ src/main/java/com/fontana/backend/log/service/LogServiceImpl.java
@@ -16,9 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 
-
-import java.util.List;
-
 import static com.fontana.backend.config.RestEndpoints.LOG;
 
 @Service
