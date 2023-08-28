8a119ae1702518942926817054430f959ff6ebd1
Oskar Pliżga <oskarplizga@outlook.com>
2023-08-28 10:25:49 +0200

    Added @Transactional, now working properly

--- src/main/java/com/fontana/backend/security/blacklist/TokenCleanupService.java
+++ src/main/java/com/fontana/backend/security/blacklist/TokenCleanupService.java
@@ -1,6 +1,7 @@
 package com.fontana.backend.security.blacklist;
 
 import com.fontana.backend.security.blacklist.repository.BlacklistedTokenRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,7 @@ import java.util.Date;
 public class TokenCleanupService {
 
     private final BlacklistedTokenRepository blacklistedTokenRepository;
-
+    @Transactional
     @Scheduled(fixedRate = 600000)//co 10 min sprawdza czy sa przedawnione tokeny
     public void cleanupBlacklistedTokens() {
         Date now = new Date();
